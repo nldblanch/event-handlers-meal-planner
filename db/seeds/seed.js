3a459527eeeed39775c87aaa9ae97c5b1f7b9816
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-08-16 11:10:39 +0100

    add code to reseed users

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -3,24 +3,36 @@ const {
   getDocs,
   addDoc,
   deleteDoc,
+  doc,
 } = require("firebase/firestore");
 const db = require("../connection");
 const data = require("../data/test-data");
+
 const seed = ({ shoppingItems, recipes, users }) => {
   const colRef = collection(db, "users");
-  const promiseArray = []
-  users.forEach(({first_name, last_name, email, password}) => {
-      promiseArray.push(addDoc(colRef, {first_name, last_name, email, password}))
-  });
-  return Promise.all(promiseArray)
-  .then((res) => {
-    console.log(res);
-    
-  })
-  .catch(err => {
-    console.log(err)
-  })
- 
+  getDocs(colRef)
+    .then((res) => {
+      const user_ids = res.docs.map((document) => {
+        return deleteDoc(doc(db, "users", document.id));
+      });
+      return Promise.all(user_ids);
+    })
+    .then(() => {
+      const promiseArray = [];
+      users.forEach(({ first_name, last_name, email, password }) => {
+        promiseArray.push(
+          addDoc(colRef, { first_name, last_name, email, password })
+        );
+      });
+      return Promise.all(promiseArray);
+    })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   //drop table lists
   //drop table recipes
   //drop table users
