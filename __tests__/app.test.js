0fa2ce6c9375a40119545543baf2283fd95d3b90
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-08-16 15:54:34 +0100

    added describe block GET

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -27,65 +27,67 @@ describe("GET: /api", () => {
 });
 
 describe("/api/lists/:username", () => {
-  it("GET: 200 it should respond with the lists (list id, isPrivate and list_name) associated for each user", () => {
-    return request(app)
-      .get("/api/lists/cityofgodshark")
-      .expect(200)
-      .then(({ body: { lists } }) => {
-        expect(lists.length).toBeGreaterThan(0);
-        lists.forEach((list) => {
-          expect(list).toMatchObject({
-            list_id: expect.any(String),
-            list_name: expect.any(String),
-            isPrivate: expect.any(Boolean),
+  describe("GET", () => {
+    it("200: it should respond with the lists (list id, isPrivate and list_name) associated for each user", () => {
+      return request(app)
+        .get("/api/lists/cityofgodshark")
+        .expect(200)
+        .then(({ body: { lists } }) => {
+          expect(lists.length).toBeGreaterThan(0);
+          lists.forEach((list) => {
+            expect(list).toMatchObject({
+              list_id: expect.any(String),
+              list_name: expect.any(String),
+              isPrivate: expect.any(Boolean),
+            });
           });
         });
-      });
-  });
-  it("GET: 200 it should respond with the lists (list id, isPrivate and list_name) associated for each user", () => {
-    return request(app)
-      .get("/api/lists/teawalrusstorm")
-      .expect(200)
-      .then(({ body: { lists } }) => {
-        expect(lists.length).toBeGreaterThan(0);
-        lists.forEach((list) => {
-          expect(list).toMatchObject({
-            list_id: expect.any(String),
-            list_name: expect.any(String),
-            isPrivate: expect.any(Boolean),
+    });
+    it("200: it should respond with the lists (list id, isPrivate and list_name) associated for each user", () => {
+      return request(app)
+        .get("/api/lists/teawalrusstorm")
+        .expect(200)
+        .then(({ body: { lists } }) => {
+          expect(lists.length).toBeGreaterThan(0);
+          lists.forEach((list) => {
+            expect(list).toMatchObject({
+              list_id: expect.any(String),
+              list_name: expect.any(String),
+              isPrivate: expect.any(Boolean),
+            });
           });
         });
-      });
+    });
   });
 });
 
 describe("/users/:username", () => {
   describe("GET", () => {
     it("200: responsds with user", () => {
-      const username = "biscuitsabloom"
+      const username = "biscuitsabloom";
       return request(app)
-      .get(`/api/users/${username}`)
-      .expect(200)
-      .then(({body: {user}}) => {
-        console.log(user);
-        
-        const {first_name, last_name, lists, password, recipes} = user
-        expect(user.username).toBe(username)
-        expect(typeof first_name).toBe("string")
-        expect(typeof last_name).toBe("string")
-        expect(Array.isArray(lists)).toBe(true)
-        expect(typeof password).toBe("string")
-        expect(Array.isArray(recipes)).toBe(true)
-      })
-    })
+        .get(`/api/users/${username}`)
+        .expect(200)
+        .then(({ body: { user } }) => {
+          console.log(user);
+
+          const { first_name, last_name, lists, password, recipes } = user;
+          expect(user.username).toBe(username);
+          expect(typeof first_name).toBe("string");
+          expect(typeof last_name).toBe("string");
+          expect(Array.isArray(lists)).toBe(true);
+          expect(typeof password).toBe("string");
+          expect(Array.isArray(recipes)).toBe(true);
+        });
+    });
     it("404: returns not found when user does not exist", () => {
-      const username = "nottherightuser"
+      const username = "nottherightuser";
       return request(app)
-      .get(`/api/users/${username}`)
-      .expect(404)
-      .then(({body: {message}}) => {
-        expect(message).toBe("User not found.")
-      })
-    })
-  })
-})
+        .get(`/api/users/${username}`)
+        .expect(404)
+        .then(({ body: { message } }) => {
+          expect(message).toBe("User not found.");
+        });
+    });
+  });
+});
