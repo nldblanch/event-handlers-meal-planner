70e45447f7f6305fca86bc35c296efdd984ffba6
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-08-16 14:26:11 +0100

    add app

---
+++ api/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const apiRouter = require("./routers/api-router");
+
+const app = express();
+
+app.use("/api", apiRouter);
+
+module.exports = app;
--- api/routers/api-router.js
+++ api/routers/api-router.js
@@ -1,11 +1,10 @@
 const apiRouter = require("express").Router();
 
-
 const { users, lists, recipes } = require("./index");
 
 apiRouter.use("/users", users);
 apiRouter.use("/lists", lists);
 apiRouter.use("/recipes", recipes);
-
+apiRouter.get("/", getEndpoints);
 
 module.exports = apiRouter;
