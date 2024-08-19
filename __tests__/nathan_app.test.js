a6129b6ba34bbd791ab90f4ee23e2da6179120df
Nathan <nldblanch@gmail.com>
2024-08-19 12:34:25 +0100

    add endpoint to patch a specific recipe

--- __tests__/nathan_app.test.js
+++ __tests__/nathan_app.test.js
@@ -38,12 +38,139 @@ describe("/api/recipes/:recipe_id", () => {
         });
     });
     it("404: returns not found when recipe id doesn't exist", () => {
-        return request(app)
+      return request(app)
         .get("/api/recipes/2000")
         .expect(404)
         .then(({ body: { message } }) => {
           expect(message).toBe("Recipe not found.");
         });
-    })
+    });
+  });
+  describe("PATCH", () => {
+    it("200: returns the recipe after being edited", () => {
+      const id = 0;
+      const patchInfo = { cook_time: 100 };
+      return request(app)
+        .patch(`/api/recipes/${id}`)
+        .send(patchInfo)
+        .expect(200)
+        .then(({ body: { recipe } }) => {
+          const {
+            recipe_id,
+            cook_time,
+            ingredients,
+            instructions,
+            prep_time,
+            recipe_name,
+          } = recipe;
+          expect(recipe_id).toBe("0");
+          expect(typeof cook_time).toBe("number");
+          expect(typeof ingredients).toBe("string");
+          expect(typeof instructions).toBe("string");
+          expect(typeof prep_time).toBe("number");
+          expect(typeof recipe_name).toBe("string");
+        });
+    });
+    it("200: patch cook_time", () => {
+      const id = 0;
+      const patchInfo = { cook_time: 100 };
+      return request(app)
+        .patch(`/api/recipes/${id}`)
+        .send(patchInfo)
+        .expect(200)
+        .then(({ body: { recipe } }) => {
+          const { recipe_id, cook_time } = recipe;
+          expect(recipe_id).toBe("0");
+          expect(cook_time).toBe(100);
+        });
+    });
+    it("200: patch ingredients", () => {
+      const id = 0;
+      const patchInfo = { ingredients: "eggs" };
+      return request(app)
+        .patch(`/api/recipes/${id}`)
+        .send(patchInfo)
+        .expect(200)
+        .then(({ body: { recipe } }) => {
+          const { recipe_id, ingredients } = recipe;
+          expect(recipe_id).toBe("0");
+          expect(ingredients).toBe("eggs");
+        });
+    });
+    it("200: patch instructions", () => {
+      const id = 0;
+      const patchInfo = { instructions: "whisk the eggs" };
+      return request(app)
+        .patch(`/api/recipes/${id}`)
+        .send(patchInfo)
+        .expect(200)
+        .then(({ body: { recipe } }) => {
+          const { recipe_id, instructions } = recipe;
+          expect(recipe_id).toBe("0");
+          expect(instructions).toBe("whisk the eggs");
+        });
+    });
+    it("200: patch prep_time", () => {
+      const id = 0;
+      const patchInfo = { prep_time: 60 };
+      return request(app)
+        .patch(`/api/recipes/${id}`)
+        .send(patchInfo)
+        .expect(200)
+        .then(({ body: { recipe } }) => {
+          const { recipe_id, prep_time } = recipe;
+          expect(recipe_id).toBe("0");
+          expect(prep_time).toBe(60);
+        });
+    });
+    it("200: patch recipe_name", () => {
+      const id = 0;
+      const patchInfo = { recipe_name: "Scrambled eggs" };
+      return request(app)
+        .patch(`/api/recipes/${id}`)
+        .send(patchInfo)
+        .expect(200)
+        .then(({ body: { recipe } }) => {
+          const { recipe_id, recipe_name } = recipe;
+          expect(recipe_id).toBe("0");
+          expect(recipe_name).toBe("Scrambled eggs");
+        });
+    });
+    it("200: patch any number of elements simultaneously", () => {
+      const id = 0;
+      const patchInfo = { cook_time: 15, recipe_name: "Scrambled eggs" };
+      return request(app)
+        .patch(`/api/recipes/${id}`)
+        .send(patchInfo)
+        .expect(200)
+        .then(({ body: { recipe } }) => {
+          const { recipe_id, cook_time, recipe_name } = recipe;
+          expect(recipe_id).toBe("0");
+          expect(cook_time).toBe(15);
+          expect(recipe_name).toBe("Scrambled eggs");
+        });
+    });
+    it("400: refuses any keys not greenlisted", () => {
+      const id = 0;
+      const patchInfo = { cook_time: 15, name: "Scrambled eggs" };
+      return request(app)
+        .patch(`/api/recipes/${id}`)
+        .send(patchInfo)
+        .expect(400)
+        .then(({ body: { message } }) => {
+          expect(message).toBe("Bad request - invalid key on object.");
+        });
+    });
+    it("400: responds bad request when no patch info given", () => {
+      const id = 0;
+      const patchInfo = {};
+      return request(app)
+        .patch(`/api/recipes/${id}`)
+        .send(patchInfo)
+        .expect(400)
+        .then(({ body: { message } }) => {
+          expect(message).toBe("Bad request - no key on object.");
+        });
+    });
   });
 });
--- api/controllers/recipes-controller.js
+++ api/controllers/recipes-controller.js
@@ -1,8 +1,18 @@
-const { fetchRecipeById } = require("../models/recipes-model");
+const { fetchRecipeById, updateRecipe } = require("../models/recipes-model");
 
 exports.getRecipeById = (request, response, next) => {
     const { recipe_id } = request.params;
     fetchRecipeById(recipe_id)
+      .then((recipe) => {
+        response.status(200).send({ recipe });
+      })
+      .catch(next);
+}
+
+exports.patchRecipe = (request, response, next) => {
+    const { recipe_id } = request.params;
+    const patchInfo = request.body;
+    updateRecipe(recipe_id, patchInfo)
       .then((recipe) => {
         response.status(200).send({ recipe });
       })
