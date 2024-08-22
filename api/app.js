9000a20e02b20192d765a90ed02d5a2a1129f424
Nathan <nldblanch@gmail.com>
2024-08-22 09:08:15 +0100

    add express cors

--- api/app.js
+++ api/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors")
 
 const apiRouter = require("./routers/api-router");
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
